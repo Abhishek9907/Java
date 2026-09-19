e065c523a02373ee7e621159403d1e350dad2c11
Abhishek9907 <abhishekmishra7766@gmail.com>
2026-09-19 17:08:40 +0000

    ArrayList

---
+++ ArrayList/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list);
+    }
+}
